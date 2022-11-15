f6384545fdeea09ad6fd57892eb10fd731d1af61
wang yu biao <wang775016074@126.com>
2022-11-15 22:51:02 +0800

    export screenshot.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const activex = require("./activex");
-const Screenshot = require("./bin/rs-qy.win32-ia32-msvc.node");
+const { ScreenShot } = require("./bin/rs-qy.win32-ia32-msvc.node");
 module.exports = {
   activex,
+  Screenshot: ScreenShot,
 };
